e761f4aef6d43fbdbdb36ef8cd272030407d65d3
Anita Zhang <anitazha@andrew.cmu.edu>
2013-09-22 23:54:52 -0400

    Fixed VSYNC and HSYNC generation
    Fixed to 640x480@60Hz reset rates

--- psx/hdmi/video.sv
+++ psx/hdmi/video.sv
@@ -1,17 +1,3 @@
-// NOTE: we're doing this in RGB with separate syncs @ 165 MHx (max).
-//       apparently in this mode we don't need input style or channel alignment
-
-//Input ID=5: An input format of RGB 4:4:4 DDR or YCbCr 4:4:4 DDR can be selected by setting the input ID (R0x15 [3:0]) to 0x5. The data bit
-//width (12, 10, or 8 bits) must be set with R0x16 [5:4]. The three input pin assignment styles are shown in the table. The Input Style can be set in
-//R0x16[3:2]. The 1st and the 2nd edge may be the rising or falling edge. The Data Input Edge is defined in R0x16 [1]. 0b1 = 1st edge rising edge; 0b0
-//= 1st edge falling edge. Pixel 0 is the first pixel of the 4:4:4 word and should be where DE starts.
-
-// Do 1920x1080 for the demo because I know what it looks like..
-// hblank - 280   hsync - 
-// vblank - 45    vsync - 
-//
-
-// rdy -- when we're ready for the next pixel, en -- when the pixel is ready
 module video(
     input  logic [23:0] data,
     input  logic        en, rst, clk,
@@ -24,13 +10,11 @@ module video(
     // used to synchronize pixel clock signal with external ready signal
     logic saw_rdy, saw_pix, pixel_rdy, pix_rdy; 
     logic [35:0] pixel, pix_data;
-    logic hs_cnt_en, hb_cnt_en, vs_cnt_en, vb_cnt_en, hp_cnt_en, vp_cnt_en;
-    logic hs_cnt_clr, hb_cnt_clr, vs_cnt_clr, vb_cnt_clr, hp_cnt_clr, vp_cnt_clr;
+    logic hb_cnt_en, vb_cnt_en, hp_cnt_en, vp_cnt_en;
+    logic hb_cnt_clr, vb_cnt_clr, hp_cnt_clr, vp_cnt_clr;
     logic [$clog2(480):0] vp_sum;
-    logic [$clog2(720):0] hp_sum;
-    logic [$clog2(20):0]  hs_sum;
-    logic [$clog2(138):0] hb_sum;
-    logic [$clog2(20):0]  vs_sum;
+    logic [$clog2(640):0] hp_sum;
+    logic [$clog2(160):0] hb_sum;
     logic [$clog2(45):0]  vb_sum;
 
     assign HDMI_DATA = pixel;
@@ -38,19 +22,15 @@ module video(
     assign pix_data = {{data[23:16], 4'b0}, {data[15:8], 4'b0}, {data[7:0], 4'b0}};
 
     // hblank/hsync counters
-    counter #(20) hsync_count (.en(hs_cnt_en), .clr(hs_cnt_clr), .rst(rst),
-                             .clk(clk_pix), .sum(hs_sum));
-    counter #(138) hblank_count (.en(hb_cnt_en), .clr(hb_cnt_clr), .rst(rst),
-                             .clk(clk_pix), .sum(hb_sum));
+    counter #(160) hblank_count (.en(hb_cnt_en), .clr(hb_cnt_clr), .rst(rst),
+                                .clk(clk_pix), .sum(hb_sum));
 
     // vblank/vsync counters
-    counter #(20) vsync_count (.en(vs_cnt_en), .clr(vs_cnt_clr), .rst(rst),
-                             .clk(clk_pix), .sum(vs_sum));
     counter #(45) vblank_count (.en(vb_cnt_en), .clr(vb_cnt_clr), .rst(rst),
-                             .clk(clk_pix), .sum(vb_sum));
+                                .clk(clk_pix), .sum(vb_sum));
 
     // height/width counters
-    counter #(720) h_pixel_count (.en(hp_cnt_en), .clr(hp_cnt_clr), .rst(rst),
+    counter #(640) h_pixel_count (.en(hp_cnt_en), .clr(hp_cnt_clr), .rst(rst),
                              .clk(clk_pix), .sum(hp_sum));
     counter #(480) v_pixel_count (.en(vp_cnt_en), .clr(vp_cnt_clr), .rst(rst),
                              .clk(clk_pix), .sum(vp_sum));
@@ -70,6 +50,7 @@ module video(
         if (rst) begin
             rdy <= 1'b1;
             saw_rdy <= 1'b0;
+            pixel_rdy <= 1'b0;
         end
         /* Ghetto clock synchronization -- saw_data tells us we asserted
          * the ready once for the current system clock, saw_pix tells us
@@ -79,8 +60,9 @@ module video(
             rdy <= 1'b1;
             saw_rdy <= 1'b1;
         end
-        else if (~saw_pix)
+        else if (~saw_pix) begin
             saw_rdy <= 1'b0;
+        end
         else begin
             rdy <= 1'b0;
         end
@@ -97,115 +79,100 @@ endmodule: video
 
 module video_fsm(
     input  logic [$clog2(480):0] vp_sum,
-    input  logic [$clog2(720):0] hp_sum,
-    input  logic [$clog2(20):0]  hs_sum,
-    input  logic [$clog2(138):0] hb_sum,
-    input  logic [$clog2(20):0]  vs_sum,
+    input  logic [$clog2(640):0] hp_sum,
+    input  logic [$clog2(160):0] hb_sum,
     input  logic [$clog2(45):0]  vb_sum,
     input  logic pixel_rdy, rst, clk_pix,
     output logic saw_pix,
-    output logic hs_cnt_en, hb_cnt_en, vs_cnt_en, vb_cnt_en, hp_cnt_en, vp_cnt_en,
-    output logic hs_cnt_clr, hb_cnt_clr, vs_cnt_clr, vb_cnt_clr, hp_cnt_clr, vp_cnt_clr,
+    output logic hb_cnt_en, vb_cnt_en, hp_cnt_en, vp_cnt_en,
+    output logic hb_cnt_clr, vb_cnt_clr, hp_cnt_clr, vp_cnt_clr,
     output logic HDMI_VSYNC, HDMI_HSYNC, HDMI_EN);
 
     enum logic [4:0] {
-        init,
-        send_frame,
-        send_syncs,
-        send_hblank,
-        wait_syncs,
-        finish_vblank,
-        finish_hblank,
-        send_hsync,
-        send_data,
-        wait_data
-    } curr_state, next_state;
+        INIT,
+        SEND_BLANK,
+        SEND_DATA,
+        WAIT_DATA
+    } curr_vstate, next_vstate, curr_hstate, next_hstate;
 
     always_ff @(posedge clk_pix, posedge rst) begin
-        if (rst)
-            curr_state <= init;
-        else
-            curr_state <= next_state;
+        if (rst) begin
+            curr_hstate <= INIT;
+            curr_vstate <= INIT;
+        end
+        else begin
+            curr_hstate <= next_hstate;
+            curr_vstate <= next_vstate;
+        end
     end
 
+    // handles horizontal syncs and pixels
     always_comb begin
-        next_state = curr_state;
+        /* Defaults */
+        next_hstate = curr_hstate;
         saw_pix = 'b0;
-        hs_cnt_en = 'b0; hb_cnt_en = 'b0;
-        vs_cnt_en = 'b0; vb_cnt_en = 'b0;
-        hp_cnt_en = 'b0; vp_cnt_en = 'b0;
-        hs_cnt_clr = 'b0; hb_cnt_clr = 'b0;
-        vs_cnt_clr = 'b0; vb_cnt_clr = 'b0;
-        hp_cnt_clr = 'b0; vp_cnt_clr = 'b0;
-        HDMI_VSYNC = 'b0; HDMI_HSYNC = 'b0; HDMI_EN = 'b0;
-
-        case (curr_state)
-            init:
-                if (pixel_rdy) next_state = send_frame;
-            send_frame: begin
-                hb_cnt_en = 'b1;
-                vb_cnt_en = 'b1;
-                hp_cnt_clr = 'b1;
-                vp_cnt_clr = 'b1;
-                // if we have sufficient blanking, send hsync
-                if (hb_sum == 'd10)
-                    next_state = send_syncs;
-            end
-            send_syncs: begin
-                hb_cnt_en = 'b1;
-                vb_cnt_en = 'b1;
-                hs_cnt_en = 'b1;
-                vs_cnt_en = 'b1;
-                HDMI_VSYNC = 'b1;
-                HDMI_HSYNC = 'b1;
-                if (hs_sum == 'd20) next_state = wait_syncs;
-            end
-            wait_syncs: begin
-                hb_cnt_en = 'b1;
-                vb_cnt_en = 'b1;
-                hs_cnt_clr = 'b1;
-                vs_cnt_clr = 'b1;
-                if (vb_sum == 'd45) next_state = finish_vblank;
-            end
-            finish_vblank: begin
-                hb_cnt_en = 'b1;
-                vb_cnt_clr = 'b1;
-                if (hb_sum == 'd138) next_state = finish_hblank;
-            end
-            finish_hblank: begin
+        hb_cnt_en = 'b0;
+        hb_cnt_clr = 'b0;
+        hp_cnt_en = 'b0;
+        hp_cnt_clr = 'b0;
+        HDMI_HSYNC = 'b1; HDMI_EN = 'b0;
+        
+        case (curr_hstate)
+            INIT: begin
                 hb_cnt_clr = 'b1;
-
-                next_state = send_data;
-            end
-            send_hblank: begin
-                hb_cnt_en = 'b1;
-                hs_cnt_clr = 'b1;
                 hp_cnt_clr = 'b1;
-                if (hb_sum == 'd138) begin
-                    vp_cnt_en = 'b1;
-                    next_state = send_data;
-                end
-                else if (hb_sum == 'd10) next_state = send_hsync;
+                if (pixel_rdy) next_hstate = SEND_BLANK;
             end
-            send_hsync: begin
+            SEND_BLANK: begin
                 hb_cnt_en = 'b1;
-                hs_cnt_en = 'b1;
-                HDMI_HSYNC = 'b1;
-                if (hs_sum == 'd20) next_state = send_hblank;
+                hp_cnt_clr = 'b1;
+                if ((hb_sum > 'd16) && (hb_sum < 'd103)) HDMI_HSYNC = 'b0;
+                else if (hb_sum == 'd160) next_hstate = SEND_DATA;
             end
-            send_data: begin
-                HDMI_EN = pixel_rdy;
+            SEND_DATA: begin
+                hb_cnt_clr = 'b1;
                 hp_cnt_en = pixel_rdy;
                 saw_pix = pixel_rdy;
-                if (hp_sum == 'd720) next_state = send_hblank;
-                else if (vp_sum == 'd480) next_state = send_frame;
-                else if (pixel_rdy) next_state = wait_data;
+                HDMI_EN = pixel_rdy;
+                if (hp_sum == 'd640) next_hstate = SEND_BLANK;
+                else if (pixel_rdy) next_hstate = WAIT_DATA;
             end
-            wait_data: begin
-                if (pixel_rdy) next_state = send_data;
+            WAIT_DATA: begin
+                if (pixel_rdy) next_hstate = SEND_DATA;
             end
         endcase
     end
+
+    // handles vertical syncs and pixels
+    always_comb begin
+        /* Defaults */
+        next_vstate = curr_vstate;
+        vb_cnt_en = 'b0;
+        vb_cnt_clr = 'b0;
+        vp_cnt_en = 'b0;
+        vp_cnt_clr = 'b0;
+        HDMI_VSYNC = 'b1;
+
+        case (curr_vstate)
+            INIT: begin
+                vb_cnt_clr = 'b1;
+                vp_cnt_clr = 'b1;
+                if (pixel_rdy) next_vstate = SEND_BLANK;
+            end
+            SEND_BLANK: begin
+                vp_cnt_clr = 'b1;
+                if (hb_sum == 'd160) vb_cnt_en = 'b1;
+                if ((vb_sum > 'd10) && (vb_sum < 'd13)) HDMI_VSYNC = 'b0;
+                else if (vb_sum == 'd45) next_vstate = SEND_DATA;
+            end
+            SEND_DATA: begin // in vertical, used to hold vblank/vsync
+                vb_cnt_clr = 'b1;
+                if (hp_sum == 'd640) vp_cnt_en = 'b1;
+                if (vp_sum == 'd480) next_vstate = SEND_BLANK;
+            end
+        endcase
+    end
+
 endmodule: video_fsm
 
 module counter(
