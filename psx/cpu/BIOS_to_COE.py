06b2577dd54e457a50575d70b971cd02d5ba781e
Arnob Mallick <amallick@andrew.cmu.edu>
2013-10-31 13:54:01 -0400

    memory module update

--- psx/cpu/BIOS_to_COE.py
+++ psx/cpu/BIOS_to_COE.py
@@ -1,21 +1,25 @@
 
 BIOS = open("SCPH7001.BIN", 'rb')
-COE  = open("bios.coe", 'w')
+COE  = open("bios_32b.coe", 'w')
 count = 0
 
+byte2bits = [''.join(['01'[i&(1<<b)>0] for b in xrange(7,-1,-1)]) for i in xrange(256)]
 
-COE.write('memory_initialization_radix=16;\n')
+COE.write('memory_initialization_radix=2;\n')
 COE.write('memory_initialization_vector=\n')
 
 while 1:
-    byte = BIOS.read(1)
+    byte = BIOS.read(4)
     if not byte:
         COE.write(';')
         break
     else:
         if (count != 0):
             COE.write(',\n')
-        COE.write(byte.encode('hex'))
+        COE.write(byte2bits[ord(byte[0])] +
+                  byte2bits[ord(byte[1])] +
+                  byte2bits[ord(byte[2])] +
+                  byte2bits[ord(byte[3])])
         count = count + 1
 
 print count
