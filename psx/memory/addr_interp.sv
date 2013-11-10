8f3e2ffa6f402183ccb410981f4eec1451baf5be
Arnob Mallick <amallick@andrew.cmu.edu>
2013-11-09 22:09:30 -0500

    SDRAM controller working finally X__X
    working reads from BIOS, main memory, scratch pad
    working write to main memory and scratch pad

--- psx/memory/addr_interp.sv
+++ psx/memory/addr_interp.sv
@@ -1,292 +1,251 @@
 
 
-
-module addr_interpreter(input logic clk, rst,
-			input logic [31:0]  addr,
-			input logic [31:0]  data_i,
-			input logic 	    ren, wen,
+module addr_interpreter(input  logic clk, rst,
+			input  logic [31:0] addr,
+			input  logic [31:0] data_i,
+			input  logic 	    ren, wen,
 			output logic 	    ack,
 			output logic [31:0] data_o,
-		   
-			/* BLOCK RAM INTERFACE */
+			output logic [ 6:0] addr_interpreter_state,
+
+			/* BLOCKRAM INTERFACE */
+			input  logic [31:0] blk_data,
 			output logic [16:0] blk_addr,
-			output logic [31:0] blk_data,
 
 			/* SDRAM INTERFACE */
-			input logic [31:0]  sd_data_o,
-			input logic 	    sd_valid,
-			input logic 	    sd_waitrequest,
+			input  logic [31:0] sd_data_o,
+			input  logic 	    sd_valid,
+			input  logic 	    sd_waitrequest,
 			output logic [24:0] sd_addr,
 			output logic [31:0] sd_data_i,
-			output logic 	    sd_rd,
-			output logic 	    sd_wr//,
-			
+			output logic 	    sd_wen,
+			output logic 	    sd_ren,
+
+			/* SCRATCH PAD BLOCKRAM INTERFACE */
+			input  logic [31:0] sc_data_o,
+			output logic [ 7:0] sc_addr,
+			output logic [31:0] sc_data_i,
+			output logic 	    sc_wen//,
+
 			/* HW REGISTER INTERFACE */
 			);
 
-
-   /* PARAMETERS */
-   /* state declarations */
-   parameter IDLE    = 4'b0001;
-   parameter READ_0  = 4'b0010;
-   parameter READ_1  = 4'b0011;
-   parameter WRITE_0 = 4'b0100;
-   parameter WRITE_1 = 4'b0101;
-   parameter WAIT    = 4'b0110;
-   parameter SCRATCH_READ  = 4'b0111;
-   parameter SCRATCH_WRITE = 4'b1000;
-   parameter HW_REG_READ   = 4'b1001;
-   parameter HW_REG_WRITE  = 4'b1010;
-   parameter PARPORT_RW    = 4'b1011;
-
-   /* constants */
-   localparam BIOS_TAG_1 = 32'h1FC00000;
-   localparam BIOS_TAG_2 = 32'h9FC00000;
-   localparam BIOS_TAG_3 = 32'hBFC00000;
+   /* PARAMETERS - state declatations */
+   localparam IDLE      = 7'b0000001;
+   localparam READ      = 7'b0000010;
+   localparam READ_ACK  = 7'b0000100;
+   localparam LATCH     = 7'b0001000;
+   localparam WRITE     = 7'b0010000;
+   localparam WRITE_ACK = 7'b0100000;
+   localparam WAIT      = 7'b1000000;
    
-   localparam MAINMEM_TAG_1  = 3'b000;       // KUSEG
-   localparam MAINMEM_TAG_2  = 3'b100;       // KSEG0
-   localparam MAINMEM_TAG_3  = 3'b101;       // KSEG1
-   localparam MAINMEM_END    = 29'h00200000; // 2Mb
-   localparam MAINMEM_OFFSET = 20'h40000;
-   
-   localparam PARPORT_START  = 32'h1F000000;
-   localparam PARPORT_END    = 32'h1F010000;
+   /* PARAMETERS - constants */
+   localparam BIOS_TAG_1 = 32'h1FC0_0000;   // start addresses for BIOS
+   localparam BIOS_TAG_2 = 32'h9FC0_0000;
+   localparam BIOS_TAG_3 = 32'hBFC0_0000;
+   localparam BIOS_SIZE  = 20'h0008_0000;
 
-   localparam SCRATCH_START  = 32'h1F800000;
-   localparam SCRATCH_END    = 32'h1F800400;
-   
-   localparam HW_REG_START  = 32'h1F801000;
-   localparam HW_REG_END    = 32'h1F803000;
+   localparam MAIN_TAG_1  = 32'h0000_0000;  // KUSEG
+   localparam MAIN_TAG_2  = 32'h8000_0000;  // KSEG0
+   localparam MAIN_TAG_3  = 32'hA000_0000;  // KSEG1
+   localparam MAIN_SIZE   = 32'h0020_0000;  // 2 megabytes
+   localparam MAIN_OFFSET = 24'h000000;
 
-   /* HARDWARE REGISTERS */
-   reg [31:0] HW_REG [0:2047];
+   localparam PPORT_TAG_1 = 32'h1F00_0000;  // parallel port start addresses
+   localparam PPORT_TAG_2 = 32'h9F00_0000;
+   localparam PPORT_TAG_3 = 32'hBF00_0000;
+   localparam PPORT_SIZE  = 32'h0001_0000;
 
-   /* SCRATCH PAD REGISTERS */
-   reg [31:0] SCRATCH_PAD [0:255];
+   localparam SCPAD_TAG_1 = 32'h1F80_0000;  // scratch pad start addresses
+   localparam SCPAD_TAG_2 = 32'h9F80_0000;
+   localparam SCPAD_SIZE  = 32'h0000_0400;
 
-   /* PARALLEL PORT */
-   reg [31:0] PARPORT_UNUSED;
-   
+   localparam HWREG_TAG_1 = 32'h1F80_1000;  // mem map I/O start addresse
+   localparam HWREG_TAG_2 = 32'h9F80_1000;
+   localparam HWREG_TAG_3 = 32'hBF80_1000;
+   localparam HWREG_SIZE  = 32'h0000_2000;
+
    /* INTERNAL LINES */
-   reg [3:0]  curr_state, next_state;
-   reg 	      curr_ack,   next_ack;
-   reg [19:0] curr_addr,  next_addr;
-   reg [31:0] curr_din,   next_din;
-   reg 	      curr_wea,   next_wea;
-   reg [7:0]  curr_SCRATCH_index, next_SCRATCH_index;
-   reg [11:0] curr_HW_REG_index, next_HW_REG_index;
-
-   reg [31:0] DATA_OUT;
-
-   wire [19:0] BIOS_ADDR, MAIN_ADDR, PHYS_ADDR;
-   wire [7:0]  SCRATCH_ADDR;
-   wire [10:0] HW_REG_ADDR;
+   reg [6:0]   curr_state,  next_state;
+   reg 	       curr_ack,    next_ack;
+   reg [24:0]  curr_addr,   next_addr;
+   reg [31:0]  curr_data_i, next_data_i;
    
-   wire        in_BIOS;
-   wire        in_MAINMEM;
-   wire        in_PARPORT;
-   wire        in_SCRATCH;
-   wire        in_HW_REG;
+   reg 	       sd_wen_out, sd_stb_out, sd_cyc_out;
+   reg 	       sc_wen_out;
+   reg [31:0]  data_out;
+   
+   wire        in_BIOS, in_MAIN, in_PPORT, in_SCPAD, in_HWREG;
+   
+   assign in_BIOS  = ((addr >= BIOS_TAG_1  & addr <  BIOS_TAG_1+BIOS_SIZE) ||
+		      (addr >= BIOS_TAG_2  & addr <  BIOS_TAG_2+BIOS_SIZE) ||
+		      (addr >= BIOS_TAG_3  & addr <  BIOS_TAG_3+BIOS_SIZE));
+   assign in_MAIN  = ((addr >= MAIN_TAG_1  & addr <  MAIN_TAG_1+MAIN_SIZE) ||
+		      (addr >= MAIN_TAG_2  & addr <  MAIN_TAG_2+MAIN_SIZE) ||
+		      (addr >= MAIN_TAG_3  & addr <  MAIN_TAG_3+MAIN_SIZE));
+   assign in_PPORT = ((addr >= PPORT_TAG_1 & addr < PPORT_TAG_1+PPORT_SIZE) ||
+		      (addr >= PPORT_TAG_2 & addr < PPORT_TAG_1+PPORT_SIZE) ||
+		      (addr >= PPORT_TAG_3 & addr < PPORT_TAG_1+PPORT_SIZE));
+   assign in_SCPAD = ((addr >= SCPAD_TAG_1 & addr < SCPAD_TAG_1+SCPAD_SIZE) ||
+		      (addr >= SCPAD_TAG_2 & addr < SCPAD_TAG_1+SCPAD_SIZE));
+   assign in_HWREG = ((addr >= HWREG_TAG_1 & addr < HWREG_TAG_1+HWREG_SIZE) ||
+		      (addr >= HWREG_TAG_2 & addr < HWREG_TAG_1+HWREG_SIZE) ||
+		      (addr >= HWREG_TAG_3 & addr < HWREG_TAG_1+HWREG_SIZE));
 
-   assign in_BIOS = (ADDR[31:19] == BIOS_TAG_1[31:19] ||
-		     ADDR[31:19] == BIOS_TAG_2[31:19] ||
-		     ADDR[31:19] == BIOS_TAG_3[31:19]  ) ? 1 : 0;
+   assign data_o = data_out;
+   assign ack    = curr_ack;
    
-   assign in_MAINMEM = ((ADDR[31:29] == MAINMEM_TAG_1 ||
-			 ADDR[31:29] == MAINMEM_TAG_2 ||
-			 ADDR[31:29] == MAINMEM_TAG_3) &&
-			ADDR[28:0] < MAINMEM_END);
+   assign sc_data_i = data_i;
+   assign sd_data_i = data_i;
+   
+   assign sc_addr = addr[9:2];
+   assign sd_addr = {5'd0, addr[20:2]};
+   
+   assign blk_addr = addr[18:2];
+   
+   assign sc_wen = sc_wen_out;
+   assign sd_wen = sd_wen_out;
+   assign sd_stb = sd_stb_out;
+   assign sd_cyc = sd_cyc_out;
+	
+   assign addr_interpreter_state = curr_state;
 
-   assign BIOS_ADDR = {3'b000, ADDR[18:2]};   
-   assign MAIN_ADDR = {1'b0, ADDR[20:2]} + 20'h40000;
-   assign PHYS_ADDR = (in_BIOS ? BIOS_ADDR : (in_MAINMEM ? MAIN_ADDR : 'd0));
-   assign HW_REG_ADDR = ADDR[13:2] - 12'h400;
-   assign SCRATCH_ADDR = ADDR[9:2];
-   
-   assign in_PARPORT = (ADDR >= PARPORT_START) && (ADDR < PARPORT_END);
-   assign in_SCRATCH = (ADDR >= SCRATCH_START) && (ADDR < SCRATCH_END);
-   assign in_HW_REG  = (ADDR >= HW_REG_START) && (ADDR < HW_REG_END);
-
-   assign PARPORT_UNUSED = 'd0;
-   
-   assign DATA_O = DATA_OUT;
-   assign ACK    = curr_ack;
-
-   assign addr_o = curr_addr;
-   assign wea_o  = curr_wea;
-   assign din_o  = curr_din;
-   
-   
    /* READ - latch value from memory */
-   always @(posedge clk, posedge rst) begin
+   always @ (posedge clk, posedge rst) begin
       if (rst) begin
-	 DATA_OUT <= 'd0;
+	 data_out <= 32'd0;
       end
       else begin
-	 if (curr_state == READ_1) begin
-	    DATA_OUT <= dout_o;
+	 if (curr_state == LATCH && in_BIOS) begin
+	    data_out <= blk_data;
 	 end
-	 else if (curr_state == SCRATCH_READ) begin
-	    DATA_OUT <= SCRATCH_PAD[curr_SCRATCH_index];
+	 else if (curr_state == LATCH && in_MAIN) begin
+	    data_out <= sd_data_o;
 	 end
-	 else if (curr_state == SCRATCH_WRITE) begin
-	    SCRATCH_PAD[curr_SCRATCH_index] <= DATA_I;
+	 else if (curr_state == LATCH && in_SCPAD) begin
+	    data_out <= sc_data_o;
 	 end
-	 else if (curr_state == HW_REG_READ) begin
-	    DATA_OUT <= HW_REG[curr_HW_REG_index];
+	 else if (curr_state == LATCH) begin
+	    data_out <= 32'd0;
 	 end
-	 else if (curr_state == HW_REG_WRITE) begin
-	    HW_REG[curr_HW_REG_index] <= DATA_I;
-	 end
-	 else if (curr_state == PARPORT_RW) begin
-	    DATA_OUT <= PARPORT_UNUSED;
-	 end
-      end // else: !if(rst)
+      end
    end
 
+   /* FSM next state logic */
+   always @ (posedge clk, posedge rst) begin
+      if (rst) begin
+	 curr_state  = IDLE;
+	 curr_ack    = 1'd0;
+      end
+      else begin
+	 curr_state  = next_state;
+	 curr_ack    = next_ack;
+      end 
+   end
    
-   /* CPU FSM next state logic */
-   always @(posedge clk, posedge rst) begin
-      if (rst) begin
-	 curr_state = IDLE;
-	 curr_ack   = 'd0;
-	 curr_addr  = 'd0;
-	 curr_din   = 'd0;
-	 curr_wea   = 'd0;
-	 curr_SCRATCH_index = 'd0;
-	 curr_HW_REG_index  = 'd0;
-      end
-      else begin
-	 if (clk) begin
-	    curr_state = next_state;
-	    curr_ack   = next_ack;
-	    curr_addr  = next_addr;
-	    curr_din   = next_din;
-	    curr_wea   = next_wea;
-	    curr_SCRATCH_index = next_SCRATCH_index;
-	    curr_HW_REG_index  = next_HW_REG_index;
-	 end
-      end
-   end
-
-   /* CPU FSM */
+   /* FSM */
    always_comb begin
-      /* DEFAULTS */
-      next_state = curr_state;
-      next_ack   = curr_ack;
-      next_addr  = curr_addr;
-      next_din   = curr_din;
-      next_wea   = curr_wea;
-      next_SCRATCH_index = curr_SCRATCH_index;
-      next_HW_REG_index  = curr_HW_REG_index;
+      /* Defaults */
+      next_state  = curr_state;
+      next_ack    = curr_ack;
       
+      sd_wen_out = 1'b0;
+      sd_ren_out = 1'b0;
+      sc_wen_out = 1'b0;
+
       case (curr_state)
+	/* wait for memory request */
 	IDLE: begin
-	   /* address in BIOS or MAIN MEMORY region */
-	   if (in_BIOS || in_MAINMEM) begin
-	      if (REN) begin
-		 next_state = READ_0;
-		 next_addr = PHYS_ADDR;
-	      end
-	      else if (WEN) begin
-		 next_state = WRITE_0;
-		 next_wea  = 1'd1;
-		 next_addr = PHYS_ADDR;
-		 next_din  = DATA_I;
-	      end
-	      else begin
-		 next_state = IDLE;
-	      end
+	   if (ren) begin
+	      next_state = READ;
 	   end
-	   /* address in SCRATCH PAD region */
-	   else if (in_SCRATCH) begin
-	      if (REN) begin
-		 next_state = SCRATCH_READ;
-		 next_SCRATCH_index = SCRATCH_ADDR;
-	      end
-	      else if (WEN) begin
-		 next_state = SCRATCH_WRITE;
-		 next_SCRATCH_index = SCRATCH_ADDR;
-	      end
+	   else if (wen) begin
+	      next_state = WRITE;
 	   end
-	   /* address in HARDWARE REGISTER region */
-	   else if (in_HW_REG) begin
-	      if (REN) begin
-		 next_state = HW_REG_READ;
-		 next_HW_REG_index = HW_REG_ADDR;
-	      end
-	      else if (WEN) begin
-		 next_state = HW_REG_WRITE;
-		 next_HW_REG_index = HW_REG_ADDR;
-	      end
-	   end
-	   /* address in PARALLEL PORT region */
-	   else if (in_PARPORT) begin
-	      if (REN || WEN) begin
-		 next_state = PARPORT_RW;
-	      end
-	   end
-	   
 	   else begin
 	      next_state = IDLE;
 	   end
-	end // case: IDLE
-
-	/* READ STATES */
-	READ_0: begin
-	   next_state = READ_1;
+	end	 
+	
+	/* READ STATE */
+	READ: begin
+	   if (in_MAIN) begin
+	      sd_ren = 1'b1;
+	      if (sd_waitrequest) begin
+		 sd_ren = 1'b0;
+		 next_state = READ;
+	      end
+	      else begin     
+		 next_state = READ_ACK;
+	      end
+	   end
+	   else begin
+	      next_state = LATCH;
+	   end
 	end
-	READ_1: begin
-	   /* sample the data */
+	READ_ACK: begin
+	   if (sd_valid) begin
+	      next_state = LATCH;
+	   end
+	   else begin
+	      next_state = READ_ACK;
+	   end
+	end
+	LATCH: begin
 	   next_state = WAIT;
-	   next_ack = 1'd1;
+	   next_ack = 1'b1;
 	end
-	SCRATCH_READ: begin
-	   next_state = WAIT;
-	   next_ack = 1'd1;
+	
+	/* WRITE STATE */
+	WRITE: begin
+	   if (in_MAIN) begin
+	      sd_wen = 1'b1;
+	      if (sd_waitrequest) begin
+		 sd_wen = 1'b0;
+		 next_state = WRITE;
+	      end
+	      else begin
+		 next_state = WAIT;
+		 next_ack = 1'b1;
+	      end
+	   end
+	   else if (in_SCPAD) begin
+	      next_state = WAIT;
+	      next_ack = 1'b1;
+	      sc_wen_out = 1'b1;
+	   end
+	   else begin
+	      /* error */
+	      next_state = IDLE;
+	   end
 	end
-	HW_REG_READ: begin
-	   next_state = WAIT;
-	   next_ack = 1'd1;
+	WRITE_ACK: begin
+	   if (sd_ack) begin
+	      next_state = WAIT;
+	      sd_stb_out = 1'b0;
+	      sd_cyc_out = 1'b0;
+	      next_ack = 1'b1;
+	   end
+	   else begin
+	      next_state = WRITE_ACK;
+	   end
 	end
-
-	/* WRITE STATES */
-	WRITE_0: begin
-	   next_state = WRITE_1;
-	   next_wea = 1'd0;
-	end
-	WRITE_1: begin
-	   /* second set of bytes written */
-	   next_state = WAIT;
-	   next_ack = 1'd1;
-	end
-	SCRATCH_WRITE: begin
-	   next_state = WAIT;
-	   next_ack = 1'd1;
-	end
-	HW_REG_WRITE: begin
-	   next_state = WAIT;
-	   next_ack = 1'd1;
-	end
-
-	/* just send back zero for parallel port */
-	PARPORT_RW: begin
-	   next_state = WAIT;
-	   next_ack = 1'd1;
-	end
-
-	/* wait for CPU to acknowledge data */
+	
+	/* WAIT for ren-wen to fall */
 	WAIT: begin
-	   if (~REN && ~WEN) begin
-	      next_ack = 1'd0;
+	   if (~ren && ~wen) begin
+	      next_ack = 1'b0;
 	      next_state = IDLE;
 	   end
 	   else begin
 	      next_state = WAIT;
 	   end
 	end
-      endcase
+      endcase // case (curr_state)
    end
    
-endmodule // mem_controller
+endmodule // addr_interpreter
+
+	
+	   
+	
--- psx/memory/test_mem_top.sv
+++ psx/memory/test_mem_top.sv
@@ -1,60 +1,118 @@
 
 
-module test_mem_top(input wire RESET, SYSCLK_P, SYSCLK_N,
-		    input logic 	BN, BW, BC, BE,
-		    //input logic [7:0] 	GPIO_DIP,
-		    output logic [7:0] 	GPIO_LED_LS);
+module memory_test
+	(input  logic        CLOCK_50,
+	 input  logic [3:0]  KEY,
+	 input  logic [17:0] SW,
+	 output logic [17:0] LEDR,
+	 output logic [8:0]  LEDG,
+	 
+	 /* SDRAM CHIP INTERFACE */
+	 output logic [12:0] DRAM_ADDR,
+	 output logic [1:0]  DRAM_BA,
+	 output logic        DRAM_CAS_N, DRAM_RAS_N, DRAM_CLK,
+	 output logic        DRAM_CKE, DRAM_CS_N, DRAM_WE_N,
+	 output logic [3:0]  DRAM_DQM,
+	 inout  wire  [31:0] DRAM_DQ);
 
    /* state definitions */
-   localparam W_INIT = 5'b00001;
-   localparam W_WAIT = 5'b00010;
-   localparam R_INIT = 5'b00100;
-   localparam R_WAIT = 5'b01000;
-   localparam DONE   = 5'b10000;
+   localparam DW_INIT = 5'b00001;
+   localparam DW_WAIT = 5'b00010;
+   localparam DR_INIT = 5'b00100;
+   localparam DR_WAIT = 5'b01000;
+   localparam DDONE   = 5'b10000;
+   localparam IR_INIT = 3'b001;
+   localparam IR_WAIT = 3'b010;
+   localparam IDONE   = 3'b100;
    
-   wire		clk, clk_33, clk_100, clk_133_0, clk_133_3, rst;
-   wire 	locked;
+   wire      clk, clk_33, clk_100, clk_133_0, clk_133_3, clk_50, rst;
+   wire      dll_locked;
+   wire      reset_controller_out;
    
-   reg [31:0] 	CPU_ADDR, CPU_ADDR_ROM, CPU_DATA_I, CPU_DATA_O, CPU_DATA_O_R;
-   reg [31:0] 	CPU_DATA;
-   reg 		CPU_REN, CPU_WEN;
-   reg 		CPU_ACK, CPU_ACK_R;
-
-   reg [4:0] 	curr_state, next_state;
-   reg 		curr_REN, next_REN;
-   reg 		curr_WEN, next_WEN;
+   reg [4:0] curr_statd, next_statd;
+   reg [2:0] curr_stati, next_stati;
+   reg 	     curr_d_ren, next_d_ren;
+   reg 	     curr_d_wen, next_d_wen;
+   reg 	     curr_i_ren, next_i_ren;
    
-   reg [7:0] 	GPIO_LED_LS_o;
-
+   reg [31:0] data_addr, data_data_i, data_data_o;
+   reg [31:0] inst_addr, inst_data_o;
+   reg        data_ren, data_wen, data_ack;
+   reg        inst_ren, inst_ack;
    
-   /* Clock Generation */
-   IBUFDS #(.DIFF_TERM("TRUE"),
-	    .IBUF_LOW_PWR("TRUE"),
-	    .IOSTANDARD("DEFAULT"))
-   clk_ibufds (.O(clk),
-	       .I(SYSCLK_P),
-	       .IB(SYSCLK_N));
+   reg [31:0] data_output, inst_output;
    
+   wire [12:0] dram_addr;
+   wire [1:0]  dram_bank;
+   wire        dram_cas_n, dram_cke, dram_cs_n; 
+   wire	       dram_ras_n, dram_we_n, dram_clk;
+   wire [3:0]  dram_dqm;
+   wire [31:0] dram_dq;
+   
+   wire [6:0]  mem_controller_state;
+   wire [6:0]  addr_interpreter_state;
+   
+   assign data_ren = curr_d_ren;
+   assign data_wen = curr_d_wen;
+   assign inst_ren = curr_i_ren;
+   
+   assign data_addr = {15'd0, SW[16:2], 2'b00};
+   assign inst_addr = 32'hBFC0_0000;
+   assign data_data_i = {SW[15:0], SW[15:0]};
+   
+   assign clk = CLOCK_50;
+   assign rst = SW[17];
+   
+   assign DRAM_CLK   = dram_clk;
+   assign DRAM_ADDR  = dram_addr;
+   assign DRAM_BA    = dram_bank;
+   assign DRAM_CAS_N = dram_cas_n;
+   assign DRAM_CKE   = dram_cke;
+   assign DRAM_CS_N  = dram_cs_n;
+   assign DRAM_DQ    = dram_dq;
+   assign DRAM_DQM   = dram_dqm;
+   assign DRAM_RAS_N = dram_ras_n;
+   assign DRAM_WE_N  = dram_we_n;
+   
+   assign LEDG[7:0]  =  ((SW[1:0] == 2'b01) ? {1'b0, mem_controller_state} :
+			((SW[1:0] == 2'b10) ? {curr_statd, curr_stati} :
+			((SW[1:0] == 2'b11) ? {1'b0, addr_interpreter_state} :
+			 {data_ren, 
+			  data_wen, 
+			  inst_ren, 
+			  1'b0, 
+			  data_ack, 
+			  inst_ack, 
+			  2'b00})));
+   
+   assign LEDR[15:0] = ((~KEY[3:0] == 4'b0001) ? data_output[15:0]  :
+			((~KEY[3:0] == 4'b0010) ? data_output[31:16] :
+			 ((~KEY[3:0] == 4'b0100) ? inst_output[15:0]  :
+			  ((~KEY[3:0] == 4'b1000) ? inst_output[31:16] : 
+			   ((~KEY[3:0] == 4'b1111) ? DRAM_DQ[31:16]     : 
+			    DRAM_DQ[15:0])))));
    
    /* memory module */
-   mem_controller mem_c(.clk(clk),
-			.clk_133_0(clk_133_0),
-			.rst(rst),
-			.zs_addr());
+   mem_controller mem_c(.clk       (CLOCK_50),
+			.clk_133_0 (clk_133_0),
+			.clk_133_3 (clk_133_3),
+			.rst       (rst),
+			.*);
+
+   /* PLL clock generator */
+   pll_50_100_133 pll(.areset (reset_controller_out),
+		      .inclk0 (CLOCK_50),
+		      .c0     (clk_33),
+		      .c1     (clk_100),
+		      .c2     (clk_133_0),
+		      .c3     (clk_133_3),
+		      .c4     (clk_50),
+		      .locked (dll_locked));
    
-
-   pll_50_100_133 (.areset (reset_controller_out),
-		   .inclk0 (clk),
-		   .c0     (clk_33),
-		   .c1     (clk_100),
-		   .c2     (clk_133_0),
-		   .c3     (clk_133_3),
-		   .locked (locked));
-
    /* reset signal synchronizer for sdram controller */
-      altera_reset_controller #(.NUM_RESET_INPUTS        (1),
-				.OUTPUT_RESET_SYNC_EDGES ("deassert"),
-				.SYNC_DEPTH              (2))
+   altera_reset_controller #(.NUM_RESET_INPUTS        (1),
+			     .OUTPUT_RESET_SYNC_EDGES ("deassert"),
+			     .SYNC_DEPTH              (2))
    rst_controller(.reset_in0  (rst),
 		  .clk        (clk),
 		  .reset_out  (reset_controller_out),
@@ -64,86 +122,125 @@ module test_mem_top(input wire RESET, SYSCLK_P, SYSCLK_N,
 		  .reset_in8  (1'b0), .reset_in9  (1'b0), .reset_in10 (1'b0),
 		  .reset_in11 (1'b0), .reset_in12 (1'b0), .reset_in13 (1'b0),
 		  .reset_in14 (1'b0), .reset_in15 (1'b0));
-
    
-   
-   assign rst = RESET;
-   assign CPU_DATA = BN ? CPU_DATA_O: CPU_DATA_O_R;
-   
-   assign GPIO_LED_LS =  (BE ? CPU_DATA[15:8] : 
-			  (BC ? CPU_DATA[23:16] :
-			   (BW ? CPU_DATA[31:24] : 
-			    (BN ? CPU_DATA[7:0] : CPU_DATA[7:0] ))));
-			 
-   assign CPU_ADDR     = 32'h00000100;
-   assign CPU_ADDR_ROM = 32'hBFC00000;
-   assign CPU_DATA_I   = 32'h12345678;
-   assign CPU_REN = curr_REN;
-   assign CPU_WEN = curr_WEN;
-   
-   /* FSM next state logic */
+   /* Latch value from reads */
    always @(posedge clk, posedge rst) begin
       if (rst) begin
-	 curr_state <= W_INIT;
-	 curr_REN <= 1'b0;
-	 curr_WEN <= 1'b0;
+	 data_output <= 32'd0;
+	 inst_output <= 32'd0;
       end
       else begin
-	 curr_state <= next_state;
-	 curr_REN <= next_REN;
-	 curr_WEN <= next_WEN;
+	 if (curr_statd == DDONE) begin
+	    data_output <= data_data_o;
+	 end
+	 if (curr_stati == IDONE) begin
+	    inst_output <= inst_data_o;
+	 end
       end
    end
    
-   /* FSM */
+   /* DATA FSM next state logic */
+   always @(posedge clk, posedge rst) begin
+      if (rst) begin
+	 curr_statd <= DW_INIT;
+	 curr_d_ren <= 1'b0;
+	 curr_d_wen <= 1'b0;
+      end
+      else begin
+	 curr_statd <= next_statd;
+	 curr_d_ren <= next_d_ren;
+	 curr_d_wen <= next_d_wen;
+      end
+   end
+   
+   /* DATA FSM */
    always_comb begin
       /* Defaults */
-      next_state = curr_state;
-      next_REN = curr_REN;
-      next_WEN = curr_WEN;
-
-      case (curr_state)
-	W_INIT: begin
-	   next_WEN = 1'b1;
-	   next_state = W_WAIT;
+      next_statd = curr_statd;
+      next_d_ren = curr_d_ren;
+      next_d_wen = curr_d_wen;
+      
+      case (curr_statd)
+	DW_INIT: begin
+	   next_d_wen = 1'b1;
+	   next_statd = DW_WAIT;
 	end
-
-	W_WAIT: begin
-	   if (CPU_ACK) begin
-	      next_WEN = 1'b0;
-	      next_state = R_INIT;
+	
+	DW_WAIT: begin
+	   if (data_ack) begin
+	      next_d_wen = 1'b0;
+	      next_statd = DR_INIT;
 	   end
 	   else begin
-	      next_state = W_WAIT;
+	      next_statd = DW_WAIT;
 	   end
 	end
-
-	R_INIT: begin
-	   if (~CPU_ACK) begin
-	      next_REN = 1'b1;
-	      next_state = R_WAIT;
+	
+	DR_INIT: begin
+	   if (~data_ack) begin
+	      next_d_ren = 1'b1;
+	      next_statd = DR_WAIT;
 	   end
 	   else begin
-	      next_state = R_INIT;
+	      next_statd = DR_INIT;
 	   end
 	end
-
-	R_WAIT: begin
-	   if (CPU_ACK) begin
-	      next_REN = 1'b0;
-	      next_state = DONE;
+	
+	DR_WAIT: begin
+	   if (data_ack) begin
+	      next_d_ren = 1'b0;
+	      next_statd = DDONE;
 	   end
 	   else begin
-	      next_state = R_WAIT;
+	      next_statd = DR_WAIT;
 	   end
 	end
-
-	DONE: begin
-	   next_state = DONE;
+	
+	DDONE: begin
+	   next_statd = DDONE;
 	end
       endcase // case (curr_state)
    end
-	   
+   
+   /* INSTRUCTION FSM next state logic */
+   always @(posedge clk, posedge rst) begin
+      if (rst) begin
+	 curr_stati <= IR_INIT;
+	 curr_i_ren <= 1'b0;
+      end
+      else begin
+	 curr_stati <= next_stati;
+	 curr_i_ren <= next_i_ren;
+      end
+   end
+   
+   /* INSTRUCTION FSM */
+   always_comb begin
+      /* Defaults */
+      next_stati = curr_stati;
+      next_i_ren = curr_i_ren;
+      
+      case (curr_stati)
+	IR_INIT: begin
+	   next_i_ren = 1'b1;
+	   next_stati = IR_WAIT;
+	end
+	
+	IR_WAIT: begin
+	   if (inst_ack) begin
+	      next_i_ren = 1'b0;
+	      next_stati = IDONE;
+	   end
+	   else begin
+	      next_stati = IR_WAIT;
+	   end
+	end
+	
+	IDONE: begin
+	   next_stati = IDONE;
+	end
+      endcase // case (curr_state)
+   end
    
 endmodule // test_mem_top
 
